6da44e761fec8a74a0b973eb4a53382b9e94cf2c
tsteele2013 <tsteele@elevenfiftyconsulting.com>
2016-03-14 21:45:17 -0400

    Cocoa pods installed

---
+++ WeatherMan/FavoritesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FavoritesViewController.swift
+//  WeatherMan
+//
+//  Created by Trevor Steele on 3/14/16.
+//  Copyright © 2016 Trevor Steele. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherMan/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WeatherMan
+//
+//  Created by Trevor Steele on 3/14/16.
+//  Copyright © 2016 Trevor Steele. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
