a10bdfe5fec2abab90dc914e01891f1d63b1a6dc
tsteele2013 <tsteele@elevenfiftyconsulting.com>
2016-03-15 17:10:03 -0400

    Slight improvements for CLLocation

--- WeatherMan/HomeViewController.swift
+++ WeatherMan/HomeViewController.swift
@@ -7,20 +7,42 @@
 //
 
 import UIKit
+import CoreLocation
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let manager = CLLocationManager()
+    var lat = 0.0
+    var lon = 0.0
+    
+    @IBAction func getWeather(sender: AnyObject) {
+        manager.requestLocation()
+    }
+    
+    @IBOutlet weak var weatherButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view, typically from a nib.
+        manager.delegate = self
+        manager.requestLocation()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            print("Found user's location: \(location.coordinate)")
+            lat = location.coordinate.latitude
+            lon = location.coordinate.longitude
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
+    
 }
 
